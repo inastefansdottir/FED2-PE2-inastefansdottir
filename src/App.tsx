d913726c8ac80d98499fdb7d7d34bc572796bbeb
Ína S Stefánsdóttir <znaste03416@stud.noroff.no>
2026-04-26 20:54:16 +0200

    Added font and the color palette to the tailwind config.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import { Routes, Route } from "react-router-dom";
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
+      <Route
+        path="/"
+        element={<h1 className="font-heading text-primary">Home</h1>}
+      />
       <Route path="/venue/:id" element={<h1>Specific Venue</h1>} />
       <Route path="/venue/edit/:id" element={<h1>Specific Venue</h1>} />
       <Route path="/register" element={<h1>Register</h1>} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#558c91",
+        secondary: "#285260",
+        accent: "#b4d8d8",
+        brownDark: "#ab9072",
+        brownLight: "#e0d7ce",
+        background: "#f7f9f9",
+      },
+      fontFamily: {
+        heading: ["Gloock", "serif"],
+        body: ["Lexend", "system-ui", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
