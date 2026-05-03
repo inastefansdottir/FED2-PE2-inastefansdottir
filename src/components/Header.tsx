b1148ecb0397ca2791f9419769f827002f91966d
Ína S Stefánsdóttir <znaste03416@stud.noroff.no>
2026-05-03 16:14:35 +0200

    Fixed the header so it shows the user info when they are logged in

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,20 +3,40 @@ import { Button } from "./Button";
 import logo from "../assets/logo-white.svg";
 
 function Header() {
+  const storedUser = localStorage.getItem("user");
+  const user = storedUser ? JSON.parse(storedUser) : null;
+
   return (
     <div className="flex justify-center bg-secondary w-full sticky top-0 px-10 z-30">
       <nav className="flex justify-between items-center w-full max-w-[1300px] py-2.5">
         <NavLink to={"/"}>
           <img src={logo} alt="Holidaze logo white" />
         </NavLink>
-        <div className="flex gap-3 font-body font-semibold">
-          <Button to={"/register"} variant="smallPrimary" size="small">
-            REGISTER
-          </Button>
-          <Button variant="light" size="small">
-            LOGIN
-          </Button>
-        </div>
+
+        {user ? (
+          <NavLink to={"/profile"} className="flex gap-3">
+            <div className="flex flex-col justify-center items-end">
+              <span className="text-background text-sm">{user.name}</span>
+              <div className="text-accent text-xs">
+                {user.venueManager ? (
+                  <span>Manager</span>
+                ) : (
+                  <span>Customer</span>
+                )}
+              </div>
+            </div>
+            <img src={user.avatar?.url} className="rounded-full w-10 h-10" />
+          </NavLink>
+        ) : (
+          <div className="flex gap-3 font-body font-semibold">
+            <Button to={"/register"} variant="smallPrimary" size="small">
+              REGISTER
+            </Button>
+            <Button variant="light" size="small">
+              LOGIN
+            </Button>
+          </div>
+        )}
       </nav>
     </div>
   );
