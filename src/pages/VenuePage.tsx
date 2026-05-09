6ebcaabdf3c917a87027fe7d9f114803c9bcf6e7
Ína S Stefánsdóttir <znaste03416@stud.noroff.no>
2026-05-09 19:53:38 +0200

    Finished making the booking cmponent and made the day picker for the create booking function.

--- src/pages/VenuePage.tsx
+++ src/pages/VenuePage.tsx
@@ -1,5 +1,9 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { Button } from "../components/Button";
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/style.css";
+import { useRef } from "react";
 import noImage from "../assets/no-image.png";
 import { getVenueById } from "../api/venues";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -20,6 +24,25 @@ function VenuePage() {
 
   const validImages = venue?.media?.filter((image) => image?.url) || [];
 
+  const [open, setOpen] = useState(false);
+  const [range, setRange] = useState<any>();
+  const [guests, setGuests] = useState<number | "">("");
+
+  const calendarRef = useRef(null);
+
+  const bookedDates = (venue?.bookings || []).map((booking) => ({
+    from: new Date(booking.dateFrom),
+    to: new Date(booking.dateTo),
+  }));
+
+  function formatDate(date: Date) {
+    return date.toLocaleDateString("en-GB", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "2-digit",
+    });
+  }
+
   useEffect(() => {
     async function fetchVenue() {
       if (!id) return;
@@ -36,14 +59,33 @@ function VenuePage() {
     fetchVenue();
   }, [id]);
 
+  // Outside click
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        calendarRef.current &&
+        !(calendarRef.current as HTMLElement).contains(event.target as Node)
+      ) {
+        setOpen(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   if (!venue) {
     return <p>Loading...</p>;
   }
 
   return (
-    <div className="px-10 mt-[50px]">
+    <div className="px-10 mt-[50px] mb-[100px]">
       <div className="max-w-[1300px] w-full">
         <div className="flex gap-5">
+          {/* Image slider */}
           <div className="flex flex-col h-[650px] gap-5 w-[229px] shrink-0">
             {Array.from({ length: 4 }).map((_, index) => {
               const image = validImages[index];
@@ -67,11 +109,12 @@ function VenuePage() {
               );
             })}
           </div>
-          <div className="relative w-[1050px] h-[650px] flex-none overflow-hidden rounded-[30px]">
+          <div className="relative max-w-[1050px] w-full h-[650px] overflow-hidden rounded-[30px]">
+            {/* Main image */}
             <img
               src={validImages[selectedImage]?.url || noImage}
               alt={validImages[selectedImage]?.alt}
-              className="absolute inset-0 w-full h-full object-cover"
+              className="w-[1050px] h-full object-cover"
             />
 
             {/* Left arrow */}
@@ -97,8 +140,9 @@ function VenuePage() {
             </button>
           </div>
         </div>
-        <div className="flex flex-1 justify-between mt-[35px]">
-          <section className="mb-[100px] max-w-[876px] w-full pr-16">
+        <div className="flex justify-between gap-10 mt-[35px]">
+          {/* Venue info */}
+          <section className="flex-1 min-w-0 pr-16">
             <div className="flex justify-between items-center">
               <h1 className="text-3xl">{venue.name}</h1>
               <div className="flex items-center self-start gap-1 text-brownDark pl-2">
@@ -182,7 +226,87 @@ function VenuePage() {
               </li>
             </ul>
           </section>
-          <div className="bg-white shadow-lg w-[413px] h-[400px] rounded-[30px]"></div>
+          {/* Booking */}
+          <div className="bg-white shadow-lg rounded-[30px] p-[35px] w-[413px]">
+            {/* Booking form */}
+            <form className="flex flex-col text-secondary gap-4">
+              {/* Price */}
+              <p className="text-primary text-xl">
+                <span className="font-semibold text-secondary">
+                  {venue.price} NOK
+                </span>
+                /Night
+              </p>
+
+              {/* Date picker */}
+              <div
+                ref={calendarRef}
+                className="relative text-sm flex flex-col border border-secondary rounded-md p-3"
+              >
+                <label htmlFor="date" className="font-semibold">
+                  CHECK-IN & CHECK-OUT
+                </label>
+
+                <div
+                  onClick={() => setOpen((prev) => !prev)}
+                  className="pt-1 text-primary cursor-pointer"
+                >
+                  {range?.from && range?.to
+                    ? `${formatDate(range.from)} - ${formatDate(range.to)}`
+                    : "Add dates"}
+                </div>
+
+                {open && (
+                  <div className="absolute top-full left-0 mt-2 bg-white shadow-lg rounded-lg p-4 z-50">
+                    <DayPicker
+                      mode="range"
+                      selected={range}
+                      onSelect={setRange}
+                      disabled={[{ before: new Date() }, ...bookedDates]}
+                    />
+                  </div>
+                )}
+              </div>
+
+              {/* Guests */}
+              <div className="text-sm flex flex-col border border-secondary rounded-md p-3">
+                <label htmlFor="guests" className="font-semibold">
+                  GUESTS
+                </label>
+
+                <input
+                  id="guests"
+                  name="guests"
+                  type="number"
+                  min={1}
+                  max={venue.maxGuests}
+                  value={guests}
+                  onChange={(e) =>
+                    setGuests(
+                      e.target.value === "" ? "" : Number(e.target.value)
+                    )
+                  }
+                  placeholder={`Max ${venue.maxGuests} guests`}
+                  className="placeholder-primary pt-1 outline-none"
+                />
+              </div>
+
+              <p className="self-end text-primary">
+                {venue.price} NOK x 5 nights
+              </p>
+
+              <hr className="border-accent w-full" />
+
+              <div className="flex justify-between font-semibold">
+                <span>Total</span>
+                <p>12500 NOK</p>
+              </div>
+
+              <Button type="submit" className="mt-3 w-full">
+                Book now
+              </Button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
